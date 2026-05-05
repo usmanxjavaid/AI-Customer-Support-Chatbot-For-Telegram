c02fcc1dcb4e523df271730e005cfaf9dd8f9f5e
usmanxjavaid <218759122+usmanxjavaid@users.noreply.github.com>
2026-05-05 20:31:45 +0500

    updated ai.py and bot.py files

--- ai.py
+++ ai.py
@@ -1,58 +1,36 @@
-# import essential libraries
-import requests # help us makes http calls to servers
-import os # let us read environment variables
+import os
+from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
-
 load_dotenv()
 
-# read the hugging facae api key from .env file
 HF_TOKEN = os.getenv('HF_TOKEN')
 
-# free ai model (Mistral-7B-instruct-v0.3) we're using on hugging face 
-API_URL = 'https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3'
+# Creates a client that talks to hugging face model
+client = InferenceClient(
+    # provider='hf-inference',
+    api_key=HF_TOKEN
+)
 
-# deine a function for getting ai reply
 def get_ai_reply(user_message: str) -> str:
-    prompt = f""" You are a friendly customer support agent.
-    Answer the customer helpfully and concisely in under 80 words.
-    if you don't know something, say so honestly. 
-    Don't hallucinate and give made-up answers to user.
-
-    customer: {user_message}
-    Agent: """
-
-    # Authorization header tells HuggingFace "Yes I have an account"
-    headers = {"Authorization": f"Bearer: {HF_TOKEN}"}
-
-    # Payload is what we send to huggingface 
-    payload = {
-        "inputs": prompt,
-        "parameters":{
-            "max_new_tokens": 150, # max length of reply
-            "temperature": 0.7, # 0 = robotic, 1 = creative
-            "return_full_text": False, # it will only return the new reply, not our prompt
-
-        }
-    }
-
-    # send request to Huggingface and wait for reply
-    response = requests.post(API_URL, headers=headers, json=payload, timeout=30)
-
-    # convert response to python dictionary
-    result = response.json()
     try:
-        # Huggingface API retruns a list of outputs, we will use the first item's text
-        if isinstance(result, list) and 'generated_text' in result[0]:
-            reply = result[0]['generated_text'].strip()
-            return reply
-            
-        # Model is cold starting, api may retrun an error when model is just waking up
-        if 'error' in result:
-            return "⏳ AI is warming up! PLease send your message again in 10 Seconds"
-    
-    except requests.exceptions.Timeout:
-        return "⏳ Taking too long to respond. Please try again."
+        # this sends your message ai model
+        response = client.chat.completions.create(
+            model="Qwen/Qwen3-235B-A22B:novita",
+            messages =[
+                {
+                    'role': 'system',
+                    'content':'You are a friendly customer support agent. Answer helpfully and concisely in under 80 words.'
+                },
+                {
+                    'role':'user',
+                    'content': user_message
+                }
+            ],
+            max_tokens=150,
+        )
+
+        # Extract the text reply from response
+        return response.choices[0].message.content.strip()
     except Exception as e:
-        return "Something went wrong. Please try again."
-
-
+        print('Error:',e)
+        return "I'm having a technical issue. Please try again shortly."
--- bot.py
+++ bot.py
@@ -1,39 +1,35 @@
 # import dependencies
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
+from telegram.ext import Application, CommandHandler, MessageHandler,ContextTypes, filters
+from dotenv import load_dotenv
+import os
+import ai
 
-# Telegram bot token
-# TELEGRAM_TOKEN = 
-
-# Below function runs when someone types /start
+# load our .env file
+load_dotenv()
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # update.effective_user is the person who sent the message
     name = update.effective_user.first_name
+    await update.message.reply_text(f"Hello {name}! I am your support bot. \n\nAsk me anything!")
 
-    # update.message.reply_text sends back reply to a person
-    await update.message.reply_text(f"Hello {name}! I am Customer support Chatbot by Nexora. How can i help you?")
-
-# Below function will run when user sens any normal message
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
-    await update.message.reply_text(f"You Said: {user_message}")
+    # show "typing..." while we wait for AI response
+    await context.bot.send_chat_action(
+        chat_id=update.effective_chat.id,
+        action='typing'
+    )
 
+    # Now we will ask for ai reply
+    reply = ai.get_ai_reply(user_message)
+    
+    await update.message.reply_text(reply)
 
-# Create the bot application object
-try:
-    app = Application.builder().token(TELEGRAM_TOKEN).build()
-    print("Bot initialized successfully!")
-except Exception as e:
-    print(f"Failed to initialize bot: {e}")
-    exit(1)
-
-# When someone types /start, call the start function
-app.add_handler(CommandHandler('start', start))
-
-# this will ignore commands (e.g. /start) and trigger normal messages 
+app = Application.builder().token(TELEGRAM_TOKEN).build()
+app.add_handler(CommandHandler('start',start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-
-
-# Start listening for messages
 app.run_polling()
+
+
+
